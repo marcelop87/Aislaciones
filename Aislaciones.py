903dc88313bbda06e3fedc4de1df3e84e5a1bad3
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-10-20 02:20:11 -0400

    Update Aislaciones.py

--- Aislaciones.py
+++ Aislaciones.py
@@ -8,33 +8,37 @@ warnings.filterwarnings('ignore')
 
 st.set_page_config(layout="wide")
 
+def load_data():
 
-conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-            r'DBQ=Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\01. REPORTES\1.1. Diarios\15 Libro de Novedades\1.-Programa\Base de Datos\LIBRONOVEDADES_be.accdb;'
-            r'Mode=Read;')
-conn = pyodbc.connect(conn_str)
-
-cursor = conn.cursor()
-aislaciones = [i.table_name for i in cursor.tables(tableType='TABLE')]
-print(aislaciones)
-
-for j in cursor.tables(tableType='VIEW'):
-    print(j.table_name)
+    conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                r'DBQ=Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\01. REPORTES\1.1. Diarios\15 Libro de Novedades\1.-Programa\Base de Datos\LIBRONOVEDADES_be.accdb;')
+    conn = pyodbc.connect(conn_str)
     
-
-lista = ['CAP', 'CAE']
-d={}
-for aislacion, nombre in zip(aislaciones[:2], lista):
-#Run SQL statement
-    cursor.execute(f'select * from [{aislacion}]')
-
-#get one row
-    one_row = cursor.fetchone()
-
-#get all rows
-    rows = cursor.fetchall()
-      
-    d[nombre] = pd.read_sql(f'select * from [{aislacion}]', conn)
+    cursor = conn.cursor()
+    aislaciones = [i.table_name for i in cursor.tables(tableType='TABLE')]
+    print(aislaciones)
+    
+    for j in cursor.tables(tableType='VIEW'):
+        print(j.table_name)
+        
+    
+    lista = ['CAP', 'CAE']
+    d={}
+    for aislacion, nombre in zip(aislaciones[:2], lista):
+    #Run SQL statement
+        cursor.execute(f'select * from [{aislacion}]')
+    
+    #get one row
+        one_row = cursor.fetchone()
+    
+    #get all rows
+        rows = cursor.fetchall()
+    
+        d[nombre] = pd.read_sql('select * from [Aislacion de Proceso]', conn)
+        
+    return d
+         
+d = load_data()
 
 df_CAP = d['CAP']
 df_CAE = d['CAE']
@@ -105,5 +109,3 @@ with st.container():
         fig = px.histogram(CAE, x='Sistema', text_auto= True, color='Sistema')
         fig.update_traces(textfont_size=20, textangle=0, textposition="outside", cliponaxis=False, showlegend=False)
         st.plotly_chart(fig)
-        
-
