00e9c290d4e79c1ebe9f3917b753fd6e3c1547ab
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-10-21 21:51:22 -0400

    Update Aislaciones.py

--- Aislaciones.py
+++ Aislaciones.py
@@ -1,27 +1,41 @@
 import streamlit as st
 import pyodbc
+import pandas as pd
 
 
-st.title('AISLACIONES DE PROCESO Y ELECTRICAS')
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-        r'DBQ=Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\01. REPORTES\1.1. Diarios\15 Libro de Novedades\1.-Programa\Base de Datos\LIBRONOVEDADES_be.accdb;'
-        r'Mode=Read;'
+
+def load_data():
+    try:
+        conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                    r'DBQ=Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\01. REPORTES\1.1. Diarios\15 Libro de Novedades\1.-Programa\Base de Datos\LIBRONOVEDADES_be.accdb;'
+                    r'Mode=Read;')
+        conn = pyodbc.connect(conn_str)
+        
+        cursor = conn.cursor()
+        aislaciones = [i.table_name for i in cursor.tables(tableType='TABLE')]
+        print(aislaciones)
+        
+        for j in cursor.tables(tableType='VIEW'):
+            print(j.table_name)
+            
+        
+        lista = ['CAP', 'CAE']
+        d={}
+        for aislacion, nombre in zip(aislaciones[:2], lista):
+        #Run SQL statement
+            cursor.execute(f'select * from [{aislacion}]')
+        
+        #get one row
+            one_row = cursor.fetchone()
+        
+        #get all rows
+            rows = cursor.fetchall()
+        
+            d[nombre] = pd.read_sql('select * from [Aislacion de Proceso]', conn)
+        
+    except Exception as e:
+        st.write("error is: {}".format(e))
+
+    return d
          
-    )
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from [Aislacion de Proceso]")
+d = load_data()
