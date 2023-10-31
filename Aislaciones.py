fb39d9285a50e4bedd4c8e0597f936c43f7e64a7
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-10-31 16:38:22 -0400

    Update Aislaciones.py

--- Aislaciones.py
+++ Aislaciones.py
@@ -48,15 +48,15 @@ if file is not None:
         col1, col2= st.columns(2)
         with col1:
             st.write('**Asilaciones de Proceso Largo Plazo**')
-            st.dataframe(CAP_largo.style.hide(axis="index"))
+            st.dataframe(CAP_largo, hide_index=True)
             st.write('**Asilaciones de Proceso Corto Plazo**')
-            st.dataframe(CAP_corto.style.hide(axis="index"))
+            st.dataframe(CAP_corto, hide_index=True)
          
         with col2:
              st.write('**Asilaciones Electricas Largo Plazo**')
-             st.dataframe(CAE_largo.style.hide(axis="index"))
+             st.dataframe(CAE_largo, hide_index=True)
              st.write('**Asilaciones Electricas Corto Plazo**')
-             st.dataframe(CAE_corto.style.hide(axis="index"))
+             st.dataframe(CAE_corto, hide_index=True)
         
    with st.container():
         
