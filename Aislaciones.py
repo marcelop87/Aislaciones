672a0dff096a50e17d2946e8c7a9c548aa9df6f4
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-10-31 11:56:23 -0400

    Update Aislaciones.py

--- Aislaciones.py
+++ Aislaciones.py
@@ -1,52 +1,67 @@
 import streamlit as st
-import pyodbc
 import pandas as pd
-
-def main():
-  
-
-  file = st.sidebar.file_uploader("Upload a database")
-
-  options = st.sidebar.radio('Pages',options = ['Data Analysis','Data visualization', 'Data Prediction'])
-
-  if file is not None:
-    data = load_data(file)
+import plotly.express as px
+st.set_page_config(layout="wide")
+   
 
 
-@st.cache_data
-def load_data(file):
-    try:
-        conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-                    r'DBQ=file;'
-                    r'Mode=Read;')
-        conn = pyodbc.connect(conn_str)
-        
-        cursor = conn.cursor()
-        aislaciones = [i.table_name for i in cursor.tables(tableType='TABLE')]
-        print(aislaciones)
-        
-        for j in cursor.tables(tableType='VIEW'):
-            print(j.table_name)
-            
-        
-        lista = ['CAP', 'CAE']
-        d={}
-        for aislacion, nombre in zip(aislaciones[:2], lista):
-        #Run SQL statement
-            cursor.execute(f'select * from [{aislacion}]')
-        
-        #get one row
-            one_row = cursor.fetchone()
-        
-        #get all rows
-            rows = cursor.fetchall()
-        
-            d[nombre] = pd.read_sql('select * from [Aislacion de Proceso]', conn)
-        
-    except Exception as e:
-        st.write("error is: {}".format(e))
+CAP = pd.read_excel(r'Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\100_SPV\output.xlsx', sheet_name='CAP')  
+CAE = pd.read_excel(r'Y:\Entity\GPI\G_200_Site_Incahuasi\05_PRODUCCION\100_SPV\output.xlsx', sheet_name='CAE') 
 
-    return d
+CAP_largo = CAP[CAP['LP']==True]
+CAP_corto = CAP[CAP['Tipo']=='Corto Plazo']
+
+CAE_largo = CAE[CAE['LP']==True]
+CAE_corto = CAE[CAE['Tipo']=='Corto Plazo']
+
+
+st.title('AISLACIONES DE PROCESO Y ELECTRICAS')
+
+with st.container():
+    
+    column1, column2 = st.columns(2)
+    
+    with column1:
+        st.header('AISLACIONES DE PROCESO')
+        col1, col2, col3 = st.columns(3)
+        
+        col1.metric("**TOTAL**", len(CAP))
+        col2.metric("**Largo Plazo**", len(CAP_largo))
+        col3.metric("**Corto Plazo**", len(CAP_corto))
+     
+    with column2:
+        st.header('AISLACIONES DE ELECTRICAS')
+        col1, col2, col3 = st.columns(3)
          
-if __name__ == "__main__":
-  main()
+        col1.metric("**TOTAL**", len(CAE))
+        col2.metric("**Largo Plazo**", len(CAE_largo))
+        col3.metric("**Corto Plazo**", len(CAE_corto))
+
+with st.expander('Ver detalle'):
+    
+    col1, col2= st.columns(2)
+    with col1:
+        st.write('**Asilaciones de Proceso Largo Plazo**')
+        st.dataframe(CAP_largo, hide_index=True)
+        st.write('**Asilaciones de Proceso Corto Plazo**')
+        st.dataframe(CAP_corto, hide_index=True)
+     
+    with col2:
+         st.write('**Asilaciones Electricas Largo Plazo**')
+         st.dataframe(CAE_largo, hide_index=True)
+         st.write('**Asilaciones Electricas Corto Plazo**')
+         st.dataframe(CAE_corto, hide_index=True)
+    
+with st.container():
+    
+    col1, col2= st.columns(2)
+    
+    with col1:
+        fig = px.histogram(CAP, x='Sistema', text_auto= True, color='Sistema')
+        fig.update_traces(textfont_size=20, textangle=0, textposition="outside", cliponaxis=False, showlegend=False)
+        st.plotly_chart(fig)
+        
+    with col2:
+        fig = px.histogram(CAE, x='Sistema', text_auto= True, color='Sistema')
+        fig.update_traces(textfont_size=20, textangle=0, textposition="outside", cliponaxis=False, showlegend=False)
+        st.plotly_chart(fig)
